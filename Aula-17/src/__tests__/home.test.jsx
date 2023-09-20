7316742109eada8b0bacd7ecb128f20a9d0f30fa
lmaoclost <renansmoliveira@gmail.com>
2023-09-19 22:17:53 -0300

    subiu aula 17

--- Aula-17/src/__tests__/home.test.jsx
+++ Aula-17/src/__tests__/home.test.jsx
@@ -1,27 +1,29 @@
 import { describe, it, expect, beforeAll, vi } from 'vitest';
-import { fireEvent, render, screen, within } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { Home } from '../pages/Home';
 
 beforeAll(() => {
   render(<Home />);
 })
 
-// Tests
 describe('Renders main page correctly', () => {
-  it('Should render the page correctly', () => {
+  it('should render the page correctly', () => {
     // Setup
-    const button = within(screen.getByText('Pesquisar'));
+    const button = screen.getByRole('button');
+
     // Expectations
     expect(button).not.toBeNull();
   });
 
-  it('Should render an alert when button clicked', () => {
+  it('should render an alert when the button is clicked', () => {
+    // Setup
     const alertMock = vi.spyOn(window, 'alert').mockImplementation();
     const button = screen.getByRole('button');
 
+    // Actions
     fireEvent.click(button);
 
     // Expectations
-    expect(alertMock).toHaveBeenCalledOnce()
+    expect(alertMock).toHaveBeenCalledOnce();
   })
 });
