03935d620ac6f0c661588ecfdcea7aafd353c3a2
lmaoclost <renansmoliveira@gmail.com>
2023-10-02 16:23:24 -0300

    Subiu aula 20

---
+++ Aula-20/src/contexts/GithubContext.jsx
@@ -0,0 +1,27 @@
+import { createContext, useEffect, useReducer } from 'react';
+import { perfilReducer } from '../reducers/perfilReducer';
+export const GithubContext = createContext();
+
+export function GithubContextProvider({ children }) {
+  const [perfis, dispatch] = useReducer(perfilReducer, [], () => {
+    const localData = localStorage.getItem('githubble-perfis');
+    return localData ? JSON.parse(localData) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('githubble-perfis', JSON.stringify(perfis));
+  }, [perfis]);
+
+  const addUsuarioALista = (user) => dispatch({ type: 'ADICIONA_PERFIL', payload: user });
+
+  const buscaPerfilPeloLogin = (perfisAntigos, perfilLogin) => {
+    const perfil = perfisAntigos.find((perfilAntigo) => perfilAntigo.login === perfilLogin);
+    return perfil;
+  }
+
+  return (
+    <GithubContext.Provider value={{ perfis, addUsuarioALista, buscaPerfilPeloLogin }}>
+      {children}
+    </GithubContext.Provider>
+  )
+}
---
+++ Aula-20/src/reducers/perfilReducer.jsx
@@ -0,0 +1,16 @@
+export const perfilReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADICIONA_PERFIL': {
+      const perfilExiste = state.filter(user => (
+        user.id === action.payload.id
+      ));
+
+      if (!perfilExiste.length) {
+        return [...state, action.payload];
+      }
+      return state;
+    }
+    default:
+      return state;
+  }
+}
